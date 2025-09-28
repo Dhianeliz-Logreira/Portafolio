82348989f40e21643d4a0d8f86dfe220cbb9211f
Dhianelizlogreira <Dhianelizlogreira@gmail.com>
2025-09-27 23:44:58 -0300

    actualizaciones

--- js/animated.js
+++ js/animated.js
@@ -64,3 +64,11 @@ window.addEventListener("load", animacion);
 //          `;    
             
 // });
+
+const cards = document.querySelectorAll(".card");
+
+cards.forEach(card => {
+  card.addEventListener("click", () => {
+    card.classList.toggle("active");
+  });
+});
